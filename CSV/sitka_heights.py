3873fceb5af6706dc124c7c6978c20ba1046945c
Vladyslav Shutkevych <vladshutkevych@gmail.com>
2025-08-30 14:31:45 +0300

    CSV Analysis + Matplotlib

--- CSV/sitka_heights.py
+++ CSV/sitka_heights.py
@@ -127,11 +127,15 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
-        high = int(row[5])
-        low = int(row[6])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[5])
+            low = int(row[6])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
         
 # Make a graph
 plt.style.use('ggplot')
